24ad773d98922c16b9ed427e44cdb2cc29e17340
Louie Cervantes <louiecervantes@wvsu.edu.ph>
2024-04-15 09:53:18 +0800

    Save

--- load_creds.py
+++ load_creds.py
@@ -24,7 +24,7 @@ def load_creds():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'client_secret.json', SCOPES)
+                ',/security/client_secret.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
